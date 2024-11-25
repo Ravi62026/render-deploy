7aa1e610b7773bdaa1b46924082cf2b69890e101
Ravi <rs1806105@gmail.com>
2024-11-25 11:46:18 +0530

    cors

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://legal-advisor-psi.vercel.app"}})
+CORS(app)
 
 
 class LegalCaseAnalyzer:
